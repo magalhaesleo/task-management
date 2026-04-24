10fa2eaf1125662b3f89996b2f5009e580814df4
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-04-24 17:55:48 -0300

    Add task management functionality: implement TaskService, configure DbContext, and update controller to fetch tasks

---
+++ backend/TaskManagementApi/Tasks/TaskService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementApi.Infrastructure;
+
+namespace TaskManagementApi.Tasks;
+
+public interface ITaskService
+{
+    Task<IEnumerable<Task>> GetTasks(CancellationToken cancellationToken);
+}
+
+public class TaskService(TaskManagementContext dbContext) : ITaskService
+{
+    public async Task<IEnumerable<Task>> GetTasks(CancellationToken cancellationToken)
+    {
+        return await dbContext.Set<Task>().AsNoTracking().ToListAsync(cancellationToken);
+    }
+}
