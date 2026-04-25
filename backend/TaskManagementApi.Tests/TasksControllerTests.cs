35fc8aa4c84da67c9d554e5357cc2fe2c813b2a3
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-04-25 16:08:48 -0300

    Add new integration tests for task retrieval, validation, and sorting in TasksController

--- backend/TaskManagementApi.Tests/TasksControllerTests.cs
+++ backend/TaskManagementApi.Tests/TasksControllerTests.cs
@@ -1,24 +1,39 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
 using AutoFixture;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TaskManagementApi.Infrastructure;
 
 namespace TaskManagementApi.Tests;
 
-public class TasksControllerTests(TaskManagementApplicationFactory factory)
-    : IClassFixture<TaskManagementApplicationFactory>
+public class TasksControllerTests
+    : IClassFixture<TaskManagementApplicationFactory>, IAsyncLifetime
 {
-    private readonly HttpClient _client = factory.CreateClient();
+    private readonly IServiceScope _scope;
+    private readonly TaskManagementContext _context;
+    private readonly HttpClient _client;
     private readonly Fixture _fixture = new();
     private const string GetAllTasksRoute = "tasks";
+
+    public TasksControllerTests(TaskManagementApplicationFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+        _context = _scope.ServiceProvider.GetRequiredService<TaskManagementContext>();
+        _client = factory.CreateClient();
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
     
     [Fact]
     public async Task Given_request_when_database_is_empty_should_return_empty_list()
     {
         // Act
-        var tasks = await _client.GetFromJsonAsync<Tasks.Task[]>(GetAllTasksRoute);
+        using var response = await _client.GetAsync(GetAllTasksRoute);
         
         // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var tasks = await response.Content.ReadFromJsonAsync<Tasks.Task[]>();
         Assert.NotNull(tasks);
         Assert.Empty(tasks);
     }
@@ -27,13 +42,15 @@ public class TasksControllerTests(TaskManagementApplicationFactory factory)
     public async Task Given_request_should_have_expected_response()
     {
         // Arrange
-        var task = _fixture.Create<Tasks.Task>();
+        var task = CreateTask();
         await SeedTasks([task]);
         
         // Act
-        var tasks = await _client.GetFromJsonAsync<Tasks.Task[]>(GetAllTasksRoute);
+        using var response = await _client.GetAsync(GetAllTasksRoute);
         
         // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var tasks = await response.Content.ReadFromJsonAsync<Tasks.Task[]>();
         Assert.NotNull(tasks);
         Assert.Equivalent(task, Assert.Single(tasks));
     }
@@ -42,29 +59,72 @@ public class TasksControllerTests(TaskManagementApplicationFactory factory)
     public async Task Given_completed_and_not_completed_tasks_should_return_completed_first()
     {
         // Arrange
-        var completedTask = _fixture.Build<Tasks.Task>()
-            .With(x => x.Completed, true)
-            .Create();
-        var notCompletedTask = _fixture.Build<Tasks.Task>()
-            .With(x => x.Completed, false)
-            .Create();
+        var completedTask = CreateTask(completed: true);
+        var notCompletedTask = CreateTask(completed: false);
         await SeedTasks([completedTask, notCompletedTask]);
         
         // Act
-        var tasks = await _client.GetFromJsonAsync<Tasks.Task[]>(GetAllTasksRoute);
+        using var response = await _client.GetAsync(GetAllTasksRoute);
         
         // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var tasks = await response.Content.ReadFromJsonAsync<Tasks.Task[]>();
         Assert.NotNull(tasks);
         Assert.Collection(tasks,
             item => Assert.Equivalent(notCompletedTask.Id, item.Id),
             item => Assert.Equal(completedTask.Id, item.Id));
     }
+    
+    [Fact]
+    public async Task Given_get_by_id_request_should_have_expected_response()
+    {
+        // Arrange
+        var taskOne = CreateTask();
+        var taskTwo = CreateTask();
+        await SeedTasks([taskOne, taskTwo]);
+        
+        // Act
+        using var response = await _client.GetAsync($"tasks/{taskTwo.Id}");
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var task = await response.Content.ReadFromJsonAsync<Tasks.Task>();
+        Assert.Equivalent(taskTwo, task);
+    }
+    
+    [Fact]
+    public async Task Given_get_by_id_request_when_id_is_invalid_should_return_not_found()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        
+        // Act
+        using var response = await _client.GetAsync($"tasks/{id}");
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    private Tasks.Task CreateTask(bool completed = false) =>
+        _fixture.Build<Tasks.Task>()
+            .With(x => x.Completed, completed)
+            .Create();
 
     private async Task SeedTasks(IEnumerable<Tasks.Task> tasks)
     {
-        using var scope = factory.Services.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<TaskManagementContext>();
-        await context.AddRangeAsync(tasks);
-        await context.SaveChangesAsync();
+        await _context.AddRangeAsync(tasks);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        var tasks = await _context.Set<Tasks.Task>().ToListAsync();
+        if (tasks.Count != 0)
+        {
+            _context.Set<Tasks.Task>().RemoveRange(tasks);
+            await _context.SaveChangesAsync();
+        }
+
+        _scope.Dispose();
     }
 }
