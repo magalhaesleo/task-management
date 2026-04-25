729e77cd7c05e6e7defd669bf4f0b602eb8c09da
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-04-25 12:20:47 -0300

    Add task creation and retrieval by ID: update TaskService, controller, and HTTP tests

--- backend/TaskManagementApi/Tasks/TaskService.cs
+++ backend/TaskManagementApi/Tasks/TaskService.cs
@@ -6,12 +6,35 @@ namespace TaskManagementApi.Tasks;
 public interface ITaskService
 {
     Task<IEnumerable<Task>> GetTasks(CancellationToken cancellationToken);
+    Task<Task> Add(AddTaskRequest taskRequest, CancellationToken cancellationToken);
+    Task<Task?> GetById(Guid id, CancellationToken cancellationToken);
 }
 
 public class TaskService(TaskManagementContext dbContext) : ITaskService
 {
     public async Task<IEnumerable<Task>> GetTasks(CancellationToken cancellationToken)
     {
-        return await dbContext.Set<Task>().AsNoTracking().ToListAsync(cancellationToken);
+        return await dbContext
+            .Set<Task>()
+            .OrderBy(x => x.IsFlagged)
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<Task> Add(AddTaskRequest taskRequest, CancellationToken cancellationToken)
+    {
+        var task = taskRequest.ToTask();
+        await dbContext.Set<Task>().AddAsync(task, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        return task;
+    }
+
+    public async Task<Task?> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        return await dbContext
+            .Set<Task>()
+            .Where(x => x.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(cancellationToken);
     }
 }
