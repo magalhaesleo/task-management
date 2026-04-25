c2503055b493577773ebe98dcd93c8e6abfa796b
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-04-25 19:17:31 -0300

    Update `Toggle` method in TaskRepository to return the updated task, improve error handling in TasksController, and make `Completed` property mutable

--- backend/TaskManagementApi/Tasks/Task.cs
+++ backend/TaskManagementApi/Tasks/Task.cs
@@ -5,6 +5,6 @@ public class Task
     public required Guid Id { get; init; }
     public required string Title { get; init; }
     public required string? Content { get; init; }
-    public required bool Completed { get; init; }
+    public bool Completed { get; set; }
     public required DateTime CreatedAt { get; init; }
 }
