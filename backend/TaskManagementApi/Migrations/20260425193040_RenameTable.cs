7f2e88480acee95a0f05d22cb62a04c7cee5ebd2
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-04-25 16:30:51 -0300

    Rename `Task` table to `Tasks` and update related migrations and DbContext configurations

---
+++ backend/TaskManagementApi/Migrations/20260425193040_RenameTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Task",
+                table: "Task");
+
+            migrationBuilder.RenameTable(
+                name: "Task",
+                newName: "Tasks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "Task");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Task",
+                table: "Task",
+                column: "Id");
+        }
+    }
+}
