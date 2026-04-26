79c4c805c4e64ce61b60531edcb286971c484036
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2026-04-26 11:35:58 -0300

    feat: add NewTask component, enhance task management, and integrate UUID package

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,4 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import api from "../services/api";
+
 export default function TaskItem({ task, onToggle }) {
+
+    const [completed, setCompleted] = useState(task.completed);
+
+    async function toggle() {
+        const data = {
+            completed
+        };
+
+        try {
+            await api.patch(`tasks/${task.id}`, data);
+            onToggle();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        toggle();
+    }, [completed]);
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+        setCompleted((prev) => !prev);
+    };
+
     return (
         <div>
             <div>
@@ -6,7 +35,7 @@ export default function TaskItem({ task, onToggle }) {
                     <input
                         type="checkbox"
                         checked={task.completed}
-                        onChange={() => onToggle(task.id)}
+                        onChange={handleChange}
                     />
                     {task.title}
                 </label>
